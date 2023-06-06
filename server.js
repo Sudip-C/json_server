29f7ffced50289313d605fe2e6111c8675fb27cb
Sudip Chowdhury <rexalex826@gmail.com>
2023-06-06 11:27:59 +0530

    new server

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const jsonserver= require("json-server");
 const server= jsonserver.create()
 const router= jsonserver.router("db.json")
 const middleware= jsonserver.defaults()
-const port =8080;
+const port =process.env.PORT ||8080;
 server.use(middleware)
-server.use(port)
+server.use(router)
 
 server.listen(port)
